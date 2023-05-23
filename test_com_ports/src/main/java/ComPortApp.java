dd1285e66a5ccd735c7a70664e7f0048318bc2da
Sergei Begletsov <snower87@yandex.ru>
2023-05-23 17:07:19 +0500

    test_com_ports, v.02. Добавил метод serialTXExample() - пример выдачи 2х байт по линии RS-485

--- test_com_ports/src/main/java/ComPortApp.java
+++ test_com_ports/src/main/java/ComPortApp.java
@@ -1,17 +1,52 @@
+import jssc.SerialPort;
+import jssc.SerialPortException;
 import jssc.SerialPortList;
 
+import java.io.UnsupportedEncodingException;
+
 public class ComPortApp {
+    public int numVer = 2;
     /**
      * getNameComPorts - выводит список доступных COM-портов
      */
     public void getNameComPorts() {
         String[] portNames = SerialPortList.getPortNames();
+        System.out.println("Метод getNameComPorts() - вывод доступных СОМ-портов");
         for (String str: portNames) {
             System.out.println(str);
         }
     }
 
+    /*
+    * serialTXExample - пример выдачи посылки из 2х байт в линию RS-485
+    */
+    public void serialTXExample() {
+        SerialPort serialPort = new SerialPort("COM10");
+        System.out.println("Метод serialTXExample() - предача сообщения по COM-порту");
+        try {
+            //Открываем порт
+            serialPort.openPort();
+
+            //Выставляем параметры
+            serialPort.setParams(SerialPort.BAUDRATE_57600,
+                    SerialPort.DATABITS_8,
+                    SerialPort.STOPBITS_1,
+                    SerialPort.PARITY_NONE);
+            for (int i = 0; i < 100; i++) {
+                //serialPort.writeString("Привет", "UTF-8");
+                byte[] bufferTX = {0x11, 0x22};
+                serialPort.writeBytes(bufferTX);
+            }
+            //Закрываем порт
+            serialPort.closePort();
+        } catch (SerialPortException e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) {
-        new ComPortApp().getNameComPorts();
+        ComPortApp comApp = new ComPortApp();
+        comApp.getNameComPorts();
+        comApp.serialTXExample();
     }
 }

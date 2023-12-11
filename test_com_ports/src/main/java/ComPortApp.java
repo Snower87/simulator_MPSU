32696dfffd2daad45313daf88a8f5e98dc1f4951
Sergei Begletsov <snower87@yandex.ru>
2023-12-11 13:44:35 +0500

    test_com_ports, v.03. Добавил метод serialRXExample() - пример приема 10 байт с тайм-аутом по линии RS-485

--- test_com_ports/src/main/java/ComPortApp.java
+++ test_com_ports/src/main/java/ComPortApp.java
@@ -1,6 +1,7 @@
 import jssc.SerialPort;
 import jssc.SerialPortException;
 import jssc.SerialPortList;
+import jssc.SerialPortTimeoutException;
 
 import java.io.UnsupportedEncodingException;
 
@@ -44,9 +45,43 @@ public class ComPortApp {
         }
     }
 
+    public void serialRXExample() {
+        SerialPort serialPort = new SerialPort("COM10");
+        System.out.println("Метод serialRXExample() - прием сообщения по COM-порту");
+
+        try {
+            //Открываем порт
+            serialPort.openPort();
+
+            //Выставляем параметры
+            serialPort.setParams(SerialPort.BAUDRATE_57600,
+                    SerialPort.DATABITS_8,
+                    SerialPort.STOPBITS_1,
+                    SerialPort.PARITY_NONE);
+            byte[] readRX = new byte[10];
+            for (int i = 0; i < 1; i++) {
+                //serialPort.writeString("Привет", "UTF-8");
+                byte[] bufferTX = {0x11, 0x22};
+                //serialPort.writeBytes(bufferTX);
+                try {
+                    readRX = serialPort.readBytes(10, 5000);
+                } catch (SerialPortTimeoutException e) {
+                    e.printStackTrace();
+                }
+            }
+            System.out.println(System.out.format("0x%02X", readRX[0]));
+            //Закрываем порт
+            serialPort.closePort();
+
+        } catch (SerialPortException e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) {
         ComPortApp comApp = new ComPortApp();
         comApp.getNameComPorts();
-        comApp.serialTXExample();
+        //comApp.serialTXExample();
+        comApp.serialRXExample();
     }
 }
